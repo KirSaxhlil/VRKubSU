22600ead464628c0d4641cd89cf504a6b9e71fe0
KirSaxhlil <dimapisarenko28@gmail.com>
2022-10-09 15:58:28 +0300

    Basic components setup

--- Source/VRKubSU/Actors/Pawns/VRPawn.cpp
+++ Source/VRKubSU/Actors/Pawns/VRPawn.cpp
@@ -9,6 +9,31 @@ AVRPawn::AVRPawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	RootComponent = DefaultSceneRoot;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(DefaultSceneRoot);
+
+	MC_Right = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("M2C_Right"));
+	MC_Right->bDisplayDeviceModel = true;
+	MC_Right->MotionSource = "Right";
+	MC_Right->SetupAttachment(DefaultSceneRoot);
+
+	MC_Left = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MC_Left"));
+	MC_Left->bDisplayDeviceModel = true;
+	MC_Left->MotionSource = "Left";
+	MC_Left->SetupAttachment(DefaultSceneRoot);
+
+	WI_Right = CreateDefaultSubobject<UWidgetInteractionComponent>(TEXT("WI_Right"));
+	WI_Right->PointerIndex = 1;
+	WI_Right->TraceChannel = ECollisionChannel::ECC_EngineTraceChannel1;
+	WI_Right->SetupAttachment(MC_Right);
+
+	WI_Left = CreateDefaultSubobject<UWidgetInteractionComponent>(TEXT("WI_Left"));
+	WI_Left->PointerIndex = 0;
+	WI_Left->TraceChannel = ECollisionChannel::ECC_EngineTraceChannel1;
+	WI_Left->SetupAttachment(MC_Left);
 }
 
 // Called when the game starts or when spawned
