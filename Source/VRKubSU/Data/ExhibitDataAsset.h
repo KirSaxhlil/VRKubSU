9b8c0bd481d7ec57e7e317bcdb4578cd4f1772f0
KirSaxhlil <dimapisarenko28@gmail.com>
2022-11-18 14:04:21 +0300

    ExhibitDataAsset class created

---
+++ Source/VRKubSU/Data/ExhibitDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ExhibitDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRKUBSU_API UExhibitDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
