a36ec941a79ecaf3b466e575491d961fbacd9a37
KirSaxhlil <dimapisarenko28@gmail.com>
2022-11-20 14:51:01 +0300

    ExhibitPortrait class created

---
+++ Source/VRKubSU/Actors/Exhibits/ExhibitPortrait.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRKubSU/Actors/Exhibits/ExhibitBase.h"
+#include "ExhibitPortrait.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRKUBSU_API AExhibitPortrait : public AExhibitBase
+{
+	GENERATED_BODY()
+	
+};
